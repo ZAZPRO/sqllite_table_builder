18916267802b3ba32bd729e9f8214798db5686f0
Daniil Mira <d.mira@medipee.com>
2023-10-31 18:26:29 +0100

    Initial commit

---
+++ example/sqllite_table_builder_example.dart
@@ -0,0 +1,6 @@
+import 'package:sqllite_table_builder/sqllite_table_builder.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/sqllite_table_builder_test.dart
@@ -0,0 +1,16 @@
+import 'package:sqllite_table_builder/sqllite_table_builder.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
